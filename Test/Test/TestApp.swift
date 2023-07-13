cdb653de1d75cfb4f663fe33d4c6b92b622546ae
nylakim <nyk7732@gmail.com>
2023-07-13 15:51:24 +0900

    commit message

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Nayun Kim on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
